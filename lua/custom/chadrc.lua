c2609db0cf905ed0844bb987a1d230e37ecf056b
Anant Narayan <anantnrg@gmail.com>
2023-08-25 14:03:20 +0530

    idk what i did

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,65 @@
+---@type ChadrcConfig 
+local M = {}
+
+M.ui = {
+  theme = "catppuccin",
+  transparency = false,
+  lsp_semantic_tokens = true,
+
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default",
+    border_color = "grey_fg",
+    selected_item_bg = "colored",
+  },
+
+  telescope = { style = "bordered" },
+
+  statusline = {
+    theme = "minimal",
+    separator_style = "round",
+    overriden_modules = nil,
+  },
+
+  tabufline = {
+    show_numbers = false,
+    enabled = true,
+    lazyload = true,
+    overriden_modules = nil,
+  },
+
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      "           ▄ ▄                   ",
+      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+  lsp = {
+    signature = {
+      disabled = false,
+      silent = true,
+    },
+  },
+}
+
+M.plugins = 'custom.plugins'
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,74 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+"rust-analyzer",
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function (_, opts)
+      require("rust-tools").setup(opts)
+    end
+  },
+  {
+     "elentok/format-on-save.nvim",
+      lazy = true,
+      cmd = { "Format" },
+      event = { "BufRead" },
+      opts = function()
+        local formatters = require("format-on-save.formatters")
+          return {
+            experiments = {
+              partial_update = "diff",
+            },
+            formatter_by_ft = {
+              json = formatters.lsp,
+              lua = formatters.stylua,
+              rust = formatters.lsp,
+              sh = formatters.shfmt,
+              toml = formatters.lsp,
+              yaml = formatters.lsp,
+            },
+           }
+     end,
+  },
+  {
+    "mfussenegger/nvim-dap"
+  },
+  {
+    "saecki/crates.nvim",
+    dependencies = "hrsh7th/nvim-cmp",
+    ft = { "rust", "toml" },
+    config = function (_, opts)
+      local crates = require("crates")
+
+      crates.setup(opts)
+      crates.show()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function ()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, { name = "crates" })
+      return M
+    end
+  }
+}
+
+return plugins
